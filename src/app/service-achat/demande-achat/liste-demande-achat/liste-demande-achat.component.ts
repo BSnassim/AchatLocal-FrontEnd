cd61985e56d7f081f2242e5befbd751b19d7d0a4
Nassim BenSalah <nassim.bensalah@esprit.tn>
2023-07-23 18:04:28 +0100

    added lists for bons de commande/demande achat/bon de sortie

--- src/app/service-achat/demande-achat/liste-demande-achat/liste-demande-achat.component.ts
+++ src/app/service-achat/demande-achat/liste-demande-achat/liste-demande-achat.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MessageService, ConfirmationService } from 'primeng/api';
+import { DemandeAchatService } from 'src/app/Services/demande-achat.service';
+import { AppBreadcrumbService } from 'src/app/main/app-breadcrumb/app.breadcrumb.service';
+import { DemandeAchat } from 'src/app/models/demande-achat';
 
 @Component({
   selector: 'app-liste-demande-achat',
   templateUrl: './liste-demande-achat.component.html',
-  styleUrls: ['./liste-demande-achat.component.scss']
+  styleUrls: ['./liste-demande-achat.component.scss'],
+  providers: [MessageService, ConfirmationService]
 })
 export class ListeDemandeAchatComponent implements OnInit {
 
-  constructor() { }
+  demandeAchatList: DemandeAchat[];
+
+  cols: any[];
+
+  constructor(
+    private breadcrumbService: AppBreadcrumbService,
+    private messageService: MessageService,
+    private confirmationService: ConfirmationService,
+    private demandeAchatService: DemandeAchatService,
+    private router: Router
+  ) {
+    this.breadcrumbService.setItems([
+      {
+        label: "Liste des demandes d'achat",
+        routerLink: ["demande/Liste-demande-achat"]
+      }
+    ])
+  }
 
   ngOnInit(): void {
+    this.cols = [
+      { field: 'demandeArticle.article.libelle', header: 'Article' },
+      { field: 'demandeArticle.demandeur', header: 'Demandeur' },
+      { field: 'magasinier', header: 'Magasinier' },
+      { field: 'demandeArticle.quantite', header: 'Quantite' },
+      { field: 'dateAchat', header: 'DateAchat' },
+    ];
+
+    this.demandeAchatService.getDemandeAchat().subscribe((data) => {
+      this.demandeAchatList = data;
+    });
   }
 
 }
